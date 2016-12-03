10956e02b20533767fa58df492e6a59714a6f1d6
chasingSublimity <blakemsager@gmail.com>
2016-12-02 19:42:24 -0600

    initial commit

---
+++ new-app.js
@@ -0,0 +1,39 @@
+// Create single-state object
+var state = {
+	items: [],
+	itemClass: []
+}
+
+// Create state-changing functions
+var addItem = function(state, itemName) {
+	state.items.push(itemName);
+}
+
+// Create state-rendering functions
+var renderList = function(state, item) {
+	var itemsHTML = state.items.map(function(item) {
+		return '\
+		<li>\
+            <span class="shopping-item">' + item + '</span>\
+            <div class="shopping-item-controls">\
+            <button class="shopping-item-toggle">\
+                <span class="button-label">check</span>\
+            </button>\
+            <button class="shopping-item-delete">\
+                <span class="button-label">delete</span>\
+            </button>\
+            </div>\
+        </li>';
+	});
+	console.log(itemsHTML)
+	$(".shopping-list").append(itemsHTML);
+};
+
+// Create Event-listeners to fire functions
+$("#js-shopping-list-form").submit(function (event) {
+	event.preventDefault();
+	addItem(state, $("#shopping-list-entry").val());
+	renderList(state, $("#shopping-list-entry").val());
+	$("#shopping-list-entry").val("");
+	$("#shopping-list-entry").focus();
+});
